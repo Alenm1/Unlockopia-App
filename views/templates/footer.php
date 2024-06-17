b97328f79f2e4da3b17bd361d812d189913f8d3d
Aldair <demianmaster2003@gmail.com>
2024-06-17 18:23:20 -0500

    mi tarea

---
+++ views/templates/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+    <div class="footer__grid">
+        <div class="footer__contenido">
+            <h3 class="footer__logo">
+                Unlockopia
+            </h3>
+
+            <p class="footer__texto">
+                Unlockopia es tienda preferida cuando deseas comprar juegos, facil y en línea.
+            </p>
+        </div>
+
+        <nav class="menu-redes">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/aldair.maqueraandrade/">
+                <span class="menu-redes__ocultar">Facebook</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://x.com/Aldair_J17">
+                <span class="menu-redes__ocultar">Twitter</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.youtube.com/@Alenm">
+                <span class="menu-redes__ocultar">YouTube</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.twitch.tv/zzz_alenm">
+                <span class="menu-redes__ocultar">Instagram</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.tiktok.com/@4z_alenm?lang=es">
+                <span class="menu-redes__ocultar">Tiktok</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://github.com/Alenm1">
+                <span class="menu-redes__ocultar">Github</span>
+            </a>
+        </nav>
+    </div>
+
+    <p class="footer__copyright">
+        Aldair Maquera
+        <span class="footer__copyright--regular">
+            - Todos los derechos reservados <?php echo date('Y'); ?>
+        </span>
+    </p>
+</footer>
---
+++ views/templates/header.php
@@ -0,0 +1,37 @@
+<header class="header">
+    <div class="header__contenedor">
+        <nav class="header__navegacion">
+            <a href="/registro" class="header__enlace">Registro</a>
+            <a href="/login" class="header__enlace">Iniciar Sesión</a>
+        </nav>
+
+        <div class="header__contenido">
+            <a href="/">
+                <h1 class="header__logo">
+                    &#60;Unlockopia />
+                </h1>
+            </a>
+
+            <p class="header__texto">Compra tus juegos</p>
+            <p class="header__texto header__texto--modalidad">A mejores precios</p>
+
+            <a href="/registro" class="header__boton">Comprar</a>
+        </div>
+    </div>
+</header>
+
+<div class="barra">
+    <div class="barra__contenido">
+        <a href="/">
+            <h2 class="barra__logo">
+                Unlockopia
+            </h2>
+        </a>
+        <nav class="navegacion">
+            <a href="/devwebcamp" class="navegacion__enlace">Tus juegos</a>
+            <a href="/paquetes" class="navegacion__enlace ">Planes</a>
+            <a href="/workshops-conferencias" class="navegacion__enlace">Shops</a>
+            <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+        </nav>
+    </div>
+</div>
